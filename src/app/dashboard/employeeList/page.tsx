4efbf67e2e9793db1b11953da2892885ef76f521
Gustavo <gusstavocta@gmail.com>
2025-05-04 23:55:09 -0500

    solucionando problemas del dashboard

--- src/app/dashboard/employeeList/page.tsx
+++ src/app/dashboard/employeeList/page.tsx
@@ -1,3 +1,4 @@
+'use client";';
 import { EmployeeFormList } from "@/components";
 
 const employeeList = () => {
--- src/app/dashboard/employeeRegister/page.tsx
+++ src/app/dashboard/employeeRegister/page.tsx
@@ -1,3 +1,4 @@
+'use client";'
 import { EmployeeFormRegister } from "@/components";
 
 const employeeRegister = () => {
