656e6cb1fe99a75c9579d48acaa21a377b72b871
Gustavo <gusstavocta@gmail.com>
2025-07-19 23:52:57 -0500

    implementacion de page adelanto sueldo

---
+++ src/app/dashboard/employeeSalaryAdvance/page.tsx
@@ -0,0 +1,11 @@
+import EmployeeSalaryAdvance from "@/components/EmployeeControl/employeeSalaryAdvance/employeeSalaryAdvance";
+
+const employeeSalaryAdvance = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <EmployeeSalaryAdvance />
+    </div>
+  );
+};
+
+export default employeeSalaryAdvance;
